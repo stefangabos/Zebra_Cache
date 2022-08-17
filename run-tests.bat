89985b1c18452cb14641fb8831eeaf658fd69d7b
stefangabos <contact@stefangabos.ro>
2022-08-17 17:31:28 +0300

    Updated coding standards and usage

--- run-tests.bat
+++ run-tests.bat
@@ -1,1 +1,1 @@
-vendor\bin\phpstan analyse Zebra_Cache.php & vendor\bin\phpcs --standard=PSR12,coding-standards.xml --report-width=130 Zebra_Cache.php
+vendor\bin\phpstan analyse & vendor\bin\phpcs --standard=coding-standards.xml
