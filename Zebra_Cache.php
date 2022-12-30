61c82e50744f7d7a7f29a8a70300152ad09ffa0a
stefangabos <contact@stefangabos.ro>
2022-12-30 13:18:17 +0200

    Bumped version number

--- Zebra_Cache.php
+++ Zebra_Cache.php
@@ -6,7 +6,7 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_Cache/ here}.
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    1.3.0 (last revision: December 29, 2022)
+ *  @version    1.3.1 (last revision: December 30, 2022)
  *  @copyright  © 2022 Stefan Gabos
  *  @license    https://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Cache
