b7394b9b827a54473a9c1f5701cc106042a8f8b3
Stefan Gabos <contact@stefangabos.ro>
2024-12-30 18:38:50 +0200

    Fixed broken link

--- examples/index.php
+++ examples/index.php
@@ -8,7 +8,7 @@ $cache = new Zebra_Cache('cache');
 // this is the file that we are going to download
 // it is a 100k test file with nothing but empty space in it
 // the name of the file will be the "key"
-$download_path = 'http://speedtest.ftp.otenet.gr/files/test100k.db';
+$download_path = 'https://examplefile.com/file-download/519';
 
 // if download was not yet cached
 if (!($response = $cache->get($download_path))) {
